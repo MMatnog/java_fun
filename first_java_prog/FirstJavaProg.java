46ec4c634068fcc8f494bac8581cb2edef271d22
Marvin Matnog <marvinmatnog@gmail.com>
2020-11-30 22:33:59 -0800

    Added FirstJavaProg

---
+++ first_java_prog/FirstJavaProg.java
@@ -0,0 +1,11 @@
+public class FirstJavaProg {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Marvin";
+        String hometown = "Los Angeles, CA";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My Hometown is " + hometown);
+    }
+}
