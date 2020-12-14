709a2856b673bcb171a058798377b75e52445ca9
Marvin Matnog <marvinmatnog@gmail.com>
2020-12-14 13:36:10 -0800

    Updated HumanController

--- spring_boot/hellohuman/src/main/java/com/marvinmatnog/hellohuman/HumanController.java
+++ spring_boot/hellohuman/src/main/java/com/marvinmatnog/hellohuman/HumanController.java
@@ -7,16 +7,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HumanController {
 	@RequestMapping("/")
-	public String hello(@RequestParam(value = "name", required = false) String name) {
-		if (name == null) {
+	public String hello(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "lastname", required = false) String lastName) {
+		if (name == null && lastName == null) {
 			return (
 					"<h1>Hello Human</h1>"
 					+ "<br>"
 					+ "<h2>This is Springboot</h2>"
 			);
-		} else {
+		} else if (lastName == null) {
 			return (
 					"<h1>Hello " + name + "</h1>"
+					+ "<br>"
+					+ "<h2>This is Springboot</h2>"
+			);
+		} else if (name == null) {
+			return (
+					"<h1>Hello " + lastName + "</h1>"
+					+ "<br>"
+					+ "<h2>This is Springboot</h2>"
+			);
+		} else {
+			return (
+					"<h1>Hello " + name + " " + lastName + "</h1>"
 					+ "<br>"
 					+ "<h2>This is Springboot</h2>"
 			);
